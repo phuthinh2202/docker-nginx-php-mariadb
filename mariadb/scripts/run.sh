2573b9ee595cd21fa580645a603440a6ee3715da
root <root@docker.me>
2016-09-16 01:13:55 +0700

    add file run database

--- mariadb/scripts/run.sh
+++ mariadb/scripts/run.sh
@@ -2,3 +2,7 @@
 # Author: thinhlp
 # Description: Start service
 
+echo "Create database and import data"
+mysql -uroot -pmatkhaudedoan -e "create database test"
+mysql -uroot -pmatkhaudedoan -e "grant all privileges on *.* to 'root'@'%' with grant option"
+mysql -uroot -pmatkhaudedoan test < /tmp/test.sql
